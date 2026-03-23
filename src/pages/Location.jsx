3272c572566edaef83e07adda53452ac84925afa
Ryan <ryanlumbis718@gmail.com>
2026-03-23 13:29:45 +0800

    beta 0.1.5

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -18,7 +18,7 @@ export default function Location() {
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/><circle cx="12" cy="10" r="3"/></svg>
               <div>
                 <strong>Address</strong>
-                <p>4PLJ+32W, Old Albay District, Legazpi City, Albay</p>
+                <p>4PRJ+32W, Old Albay District, Legazpi City, Albay</p>
               </div>
             </div>
             <div className="location-detail">
@@ -39,7 +39,7 @@ export default function Location() {
 
             <div className="location-page__btns">
               <a
-                href="https://maps.google.com/?q=Jazsam+Coffee+Legazpi+City+Albay"
+                href="https://maps.google.com/?q=4PRJ%2B32W+Old+Albay+District+Legazpi+City+Albay"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-primary"
@@ -57,7 +57,7 @@ export default function Location() {
               loading="lazy"
               allowFullScreen
               referrerPolicy="no-referrer-when-downgrade"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.3!2d123.7541!3d13.1391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sLegazpi+City%2C+Albay!5e0!3m2!1sen!2sph!4v1700000000000!5m2!1sen!2sph"
+              src="https://maps.google.com/maps?q=4PRJ%2B32W+Old+Albay+District+Legazpi+City+Albay+Philippines&output=embed"
             />
           </div>
         </div>
